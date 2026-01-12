4f6747f0383ec08bc02e8a1a659f1397538d8f47
Al Auad <auadix@gmail.com>
2026-01-11 21:56:10 -0500

    Bump version to 2.9.1

--- kia-vehicle-card.js
+++ kia-vehicle-card.js
@@ -1,7 +1,7 @@
 /**
  * Kia Vehicle Card for Home Assistant
  * A modern, comprehensive card for Kia/Hyundai vehicles
- * Version: 2.9.0
+ * Version: 2.9.1
  *
  * Features:
  * - MDI icons (no emojis)
